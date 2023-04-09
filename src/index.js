3126725fd72b46ec164b132952e44122e9b180e0
Bikash Shaw <1985.bikash.shaw@gmail.com>
2023-04-09 18:14:52 +0530

    Action type created

--- src/index.js
+++ src/index.js
@@ -4,19 +4,24 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 // Default State
 var defaultState = 0;
+
+// Action_Types
+const DEPOSIT = "DEPOSIT";
+const WITHDRAW = "WITHDRAW";
 
 // Reducers
 const balanceReducer = (state = defaultState, action) => {
   console.log("Reducer Invoked..", action);
   switch (action.type) {
-    case "DEPOSIT":
-      return state + 100;
+    case DEPOSIT:
+      return state + action.payload.amount;
 
-    case "WITHDRAW":
-      return state - 100;
+    case WITHDRAW:
+      return state - action.payload.amount;
 
     default:
       return state;
@@ -24,15 +29,18 @@ const balanceReducer = (state = defaultState, action) => {
 };
 
 // Store
-const store = createStore(balanceReducer);
-console.log(store.getState());
+const store = createStore(balanceReducer, composeWithDevTools());
+store.subscribe(() => {
+  console.log(store.getState());
+});
 
 // Dispatch
-store.dispatch({ type: "abc" });
-console.log(store.getState());
 
-store.dispatch({ type: "DEPOSIT" });
-console.log(store.getState());
+store.dispatch({ type: DEPOSIT, payload: { amount: 500 } });
+
+store.dispatch({ type: DEPOSIT, payload: { amount: 1000 } });
+
+store.dispatch({ type: WITHDRAW, payload: { amount: 200 } });
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
