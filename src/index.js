27403a9eb36c9064e356b94d1cb51f0e0734db98
Bikash Shaw <1985.bikash.shaw@gmail.com>
2023-04-09 14:57:34 +0530

    reducer, store and dispatch implemented

--- src/index.js
+++ src/index.js
@@ -1,17 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { createStore } from "redux";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// Default State
+var defaultState = 0;
+
+// Reducers
+const balanceReducer = (state = defaultState, action) => {
+  console.log("Reducer Invoked..", action);
+  switch (action.type) {
+    case "DEPOSIT":
+      return state + 100;
+
+    case "WITHDRAW":
+      return state - 100;
+
+    default:
+      return state;
+  }
+};
+
+// Store
+const store = createStore(balanceReducer);
+console.log(store.getState());
+
+// Dispatch
+store.dispatch({ type: "abc" });
+console.log(store.getState());
+
+store.dispatch({ type: "DEPOSIT" });
+console.log(store.getState());
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
